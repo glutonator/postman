5f8cb229c6004740a1522ed7ca557c9b366e6cc6
Glutek <glutonator@gmail.com>
2018-12-27 21:00:19 +0100

    todo

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -17,6 +17,8 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String... args) throws Exception {
+        //todo: dla przystej liczby wierchołków od 8 w zwyż jest jakiś błað i czasem generuje się nie graf eulerowski
+        //todo: dla nieprzystej liczby jest okey chyba zawsze
         Postman postman = new Postman(CustomGraph.createComleteGraphUndireted(5),100);
 //        Postman postman = new Postman(SimpleTests.createDirectedGraphFromBookMultigraphCustomEdge());
         postman.auxiliaryAlg();
